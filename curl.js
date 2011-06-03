97ed0bc42ef3e2d31b39d8adacf762fcf4fc5e97
gareth <gareth@PaloAlto.(none)>
2011-06-02 21:11:27 -0400

    Initial commit

---
+++ curl.js
@@ -0,0 +1,27 @@
+function textInput(name, value) {
+  return "<input type='text' name='" + name + "' value='" + value + "'/>";
+}
+
+$(document).ready(function() {
+  $('#submit').click(function() {
+    url = $('#url-input').val();
+    method = $('#method-input').val();
+
+    $('#post-form').attr('method', method);
+    $('#post-form').attr('action', url);
+    
+    $('.key-val-pair').each(function(index, element) {
+      name = $('.param', element).val();
+      value = $('.value', element).val();
+
+      $('#post-form').append(textInput(name, value));
+    });
+
+    $('#post-submit').click()
+  });
+
+  $('#new-param').click(
+    function() {
+      $('#input-form').append("<p class='key-val-pair'><input type='text' class='param' value='param'/><input type='text' class='value' value='value'/></p>");
+  });
+});
